0274757f0df2d4632fe89fb7755f536de6d25def
Sahar Pk <sahar.pk88@gmail.com>
2021-02-04 19:00:58 +0100

    Added the Unit tests.

--- src/com/example/spk/DeletionTest.java
+++ src/com/example/spk/DeletionTest.java
@@ -27,5 +27,16 @@ class Tests {
         assertEquals(1,nrDeletion.numberOfDeletion("PpppkkKk"));
     }
 
-
+    @Test
+    public void equalSideArraysTest(){
+        assertEquals(3,equalSideArrays.findEvenIndex(new int[] {1,2,3,4,3,2,1}));
+        assertEquals(0,equalSideArrays.findEvenIndex(new int[]{20,10,-80,10,10,15,35}));
+        assertEquals(1,equalSideArrays.findEvenIndex(new int[]{1,100,50,-51,1,1}));
+        assertEquals(1,equalSideArrays.findEvenIndex(new int[] {1,100,50,-51,1,1}));
+        assertEquals(-1,equalSideArrays.findEvenIndex(new int[] {1,2,3,4,5,6}));
+        assertEquals(3,equalSideArrays.findEvenIndex(new int[] {20,10,30,10,10,15,35}));
+        assertEquals(-1,equalSideArrays.findEvenIndex(new int[] {-8505, -5130, 1926, -9026}));
+        assertEquals(1,equalSideArrays.findEvenIndex(new int[] {2824, 1774, -1490, -9084, -9696, 23094}));
+        assertEquals(6,equalSideArrays.findEvenIndex(new int[] {4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}));
+    }
 }
